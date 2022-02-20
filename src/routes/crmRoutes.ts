6749b973a9c5bc9af6dce229caef636ef22a50a3
Nguyen Quoc Dat <nqd.1912@gmail.com>
2022-02-20 13:29:29 +0700

    Variables

--- src/routes/crmRoutes.ts
+++ src/routes/crmRoutes.ts
@@ -1,6 +1,10 @@
 import {Request, Response} from "express";
 
+import { BookController } from "../controllers/bookController";
+
+
 export class Routes {       
+    public bookController: BookController = new BookController();
     public routes(app): void {          
         app.route('/')
         .get((req: Request, res: Response) => {            
@@ -8,5 +12,19 @@ export class Routes {
                 message: 'GET request successfulll!!!!'
             })
         })               
+
+        // GET ALL BOOKS
+        app.route('/books')
+        .get(this.bookController.getBooks)
+        .post(this.bookController.addNewBook)
+        // GET DETAIL BOOK
+        app.route('/books/:bookId')
+        .get(this.bookController.getBookWithID)
+        .put(this.bookController.updateBook)
+        .delete(this.bookController.deleteBook)
+
+
+
+
     }
 }
